2efb7ef30a22c9cbf4e21cbeb028577c7835b4f7
shreyas gs <shreyascivil05@gmail.com>
2025-06-23 07:26:25 +0530

    emovve items

--- database/migration_add_user_role.sql
+++
@@ -1,9 +0,0 @@
--- Migration to add 'USER' role to the existing role check constraint
--- Run this script to update the existing database
-
--- Drop the existing check constraint
-ALTER TABLE my_group_user DROP CONSTRAINT IF EXISTS my_group_user_role_check;
-
--- Add the new check constraint with 'USER' role included
-ALTER TABLE my_group_user ADD CONSTRAINT my_group_user_role_check 
-CHECK (role IN ('SUPER_ADMIN', 'CORPORATE', 'BRANCH', 'USER'));
